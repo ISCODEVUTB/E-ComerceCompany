7ba01970ebf22cc521f6e04cc4ecc27fe0472bf0
Kathy Otero <kotero@utb.edu.co>
2025-05-26 13:37:47 -0500

    Update test_crear_usuario.py

--- test/test_crear_usuario.py
+++ test/test_crear_usuario.py
@@ -1,6 +1,6 @@
 import pytest
 from sqlalchemy.orm import Session
-from database import SessionLocal
+from backend.app.database import SessionLocal
 from models import Usuario
 from schemas import UsuarioCreate
 import crud
