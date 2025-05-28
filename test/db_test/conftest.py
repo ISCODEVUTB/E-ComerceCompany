fd21eb2c46632499c21794d1916c21ed4c2b0248
Hawasly7 <jesusjulio7622@gmail.com>
2025-05-28 04:00:15 +0000

    Hawasly TestDatabase 27/05 (1)

--- test/db_test/conftest.py
+++ test/db_test/conftest.py
@@ -1,2 +1,20 @@
-import os, sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# test/conftest.py
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from backend.app.logic.database import Base  # Importa tus modelos
+
+SQLALCHEMY_DATABASE_URL = mssql+pyodbc://talento:cartagena@nodossolutions.com:1435/ecommerce?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+TestingSessionLocal = sessionmaker(bind=engine)
+
+@pytest.fixture(scope="function")
+def db():
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        Base.metadata.drop_all(bind=engine)
