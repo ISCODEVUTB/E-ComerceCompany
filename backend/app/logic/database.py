632733f9ba6776f780c98c78bbc1366c28ee91b2
Hawasly7 <jesusjulio7622@gmail.com>
2025-05-28 22:33:36 -0500

    Update database.py

--- backend/app/logic/database.py
+++ backend/app/logic/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from decouple import config
 
-SQLALCHEMY_DATABASE_URL = config("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = config("mssql+pyodbc://talento:cartagena@nodossolutions.com:1435/ecommerce?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
