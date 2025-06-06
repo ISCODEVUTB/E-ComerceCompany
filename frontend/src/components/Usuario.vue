53a58fa1c90a437078388b5828471cfd13bd288f
Hawasly7 <jesusjulio7622@gmail.com>
2025-06-06 13:41:41 +0000

    Users_link

--- frontend/src/components/Usuario.vue
+++ frontend/src/components/Usuario.vue
@@ -120,7 +120,7 @@ export default {
         return;
       }
       try {
-        const res = await fetch('https://e-comercecompany-production.up.railway.app/productos', {
+        const res = await fetch('https://e-comercecompany-production.up.railway.app/usuarios', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
