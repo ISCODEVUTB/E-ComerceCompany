2ef05b78b79f1cc1cc2dc3a295c09bfbf1ef25ad
Hawasly7 <jesusjulio7622@gmail.com>
2025-06-06 07:42:51 +0000

    FINAL

--- backend/app/logic/database.py
+++ backend/app/logic/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from decouple import config
 
-SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://talento:cartagena@nodossolutions.com:1435/ecommerce?driver=ODBC+Driver+17+for+SQL+Server&TrustServerCertificate=yes"
+SQLALCHEMY_DATABASE_URL = "mssql+pyodbc://talento:cartagena@nodossolutions.com:1435/ecommerce?driver=ODBC+Driver+18+for+SQL+Server&TrustServerCertificate=yes"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
